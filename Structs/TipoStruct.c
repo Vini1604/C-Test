27852c0c03a471affe657fb6d56e37d74c98dcef
Vini1604 <vinicius.oliveira1998@hotmail.com>
2022-09-30 11:41:40 -0300

    Quantidade de notas via usuario 'Struct'

--- Structs/TipoStruct.c
+++ Structs/TipoStruct.c
@@ -1,48 +1,56 @@
 #include<stdio.h>
 #include<string.h>
-#define qteNotas 4
+#include<stdlib.h>
+
 struct Aluno {
     char matricula[8];
     char nomeAluno[50];
-    double notas[qteNotas];
+    double *notas;
 
 };
 
-void InsereNotas(struct Aluno *a1) {
-    for(int i = 0; i < qteNotas; i++){
+void InsereNotas(struct Aluno *a1, int n) {
+    for(int i = 0; i < n; i++){
         printf("Digite a nota P%d do Aluno: ", i + 1);
         scanf("%lf",&a1->notas[i]);
-    }    
+    }
 }
 
-void ImprimeNotas(double notas[]) {
-    for (int i = 0; i < qteNotas; i++){
+void ImprimeNotas(double notas[], int n) {
+    for (int i = 0; i < n; i++){
         printf("Nota P%d = %.2lf\n", i + 1, notas[i]);
     }
-      
 }
 
 
-double CalculaMedia(double notas[]) {
+double CalculaMedia(double notas[], int n) {
     double soma = 0.0;
-    for (int i = 0; i < qteNotas; i++)
+    for (int i = 0; i < n; i++)
     {
         soma += notas[i];
     }
-    return soma / qteNotas;
+    return soma / n;
+}
+
+void CriaEspacoNotas(struct Aluno *a, int n){
+    a->notas = (double*) malloc(n * sizeof(double));
 }
 int main(void) {
 
+    int n;
     double mediaFinal;
     struct Aluno a1;
     printf("Digite a matricula do Aluno: ");
     gets(a1.matricula);
     printf("Digite o nome do Aluno: ");
     gets(a1.nomeAluno);
-    InsereNotas(&a1);
-    mediaFinal = CalculaMedia(a1.notas);
+    printf("Digite a quantidade de notas a serem inseridas: ");
+    scanf("%d", &n);
+    CriaEspacoNotas(&a1, n);
+    InsereNotas(&a1, n);
+    mediaFinal = CalculaMedia(a1.notas, n);
     printf("\n\nAluno #%s:\n", a1.matricula);
     printf("Nome: %s\n", a1.nomeAluno);
-    ImprimeNotas(a1.notas);
+    ImprimeNotas(a1.notas, n);
     printf("Media: %.2lf", mediaFinal);
 }
